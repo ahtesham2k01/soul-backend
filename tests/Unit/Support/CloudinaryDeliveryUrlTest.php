9bfe48c8f18fc447f8d874c3f5b279c7c0ee0dad
Ahtesham Shah <77572788+ahtesham2k01@users.noreply.github.com>
2026-09-10 11:43:15 +0500

    Align signed delivery test with SHA-256

--- tests/Unit/Support/CloudinaryDeliveryUrlTest.php
+++ tests/Unit/Support/CloudinaryDeliveryUrlTest.php
@@ -9,11 +9,15 @@ class CloudinaryDeliveryUrlTest extends TestCase
 {
     public function test_it_generates_an_authenticated_delivery_signature_without_exposing_secret(): void
     {
-        config(['soul.media.cloudinary.cloud_name' => 'demo', 'soul.media.cloudinary.api_secret' => 'abcd']);
+        config([
+            'soul.media.cloudinary.cloud_name' => 'demo',
+            'soul.media.cloudinary.api_secret' => 'abcd',
+            'soul.media.cloudinary.response_signature_algorithm' => 'sha256',
+        ]);
 
         $url = app(CloudinaryDeliveryUrl::class)->forAuthenticatedImage('sample-authenticated', 'png');
 
-        $this->assertSame('https://res.cloudinary.com/demo/image/authenticated/s--QqLhlx8M--/sample-authenticated.png', $url);
+        $this->assertSame('https://res.cloudinary.com/demo/image/authenticated/s--PkHCb2Jt--/sample-authenticated.png', $url);
         $this->assertStringNotContainsString('abcd', $url);
     }
 }
